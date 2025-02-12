52acfb30980ec94288c8d0a29d033b1cc7cd223e
ben-madley-mt <91620321+ben-madley-mt@users.noreply.github.com>
2025-02-12 10:33:09 +0000

    Fix description

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/model/response/ContactDetails.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/model/response/ContactDetails.kt
@@ -44,7 +44,7 @@ data class ContactDetails(
   @Schema(description = "Whether the contact is a staff member", example = "false", nullable = false)
   var isStaff: Boolean = false,
 
-  @Schema(description = "The date the contact deceased, if known", example = "true")
+  @Schema(description = "Whether the contact is deceased", example = "true")
   val isDeceased: Boolean,
 
   @Schema(description = "The date the contact deceased, if known", example = "1980-01-01", nullable = true)
