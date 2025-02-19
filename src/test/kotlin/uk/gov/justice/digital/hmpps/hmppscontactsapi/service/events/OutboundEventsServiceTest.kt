88df5b255d5215d5e36353d58f60df451f534a46
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-02-19 13:51:54 +0000

    PRC-426 - create data model, sync , migration for domestic status - fix and add more tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/service/events/OutboundEventsServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/service/events/OutboundEventsServiceTest.kt
@@ -320,7 +320,14 @@ class OutboundEventsServiceTest {
   }
 
   @ParameterizedTest
-  @EnumSource(OutboundEvent::class)
+  @EnumSource(
+    value = OutboundEvent::class,
+    mode = EnumSource.Mode.EXCLUDE,
+    names = [
+      "PRISONER_DOMESTIC_STATUS_CREATED",
+      "PRISONER_DOMESTIC_STATUS_UPDATED",
+      "PRISONER_DOMESTIC_STATUS_DELETED"]
+  )
   fun `should trap exception sending event`(event: OutboundEvent) {
     featureSwitches.stub { on { isEnabled(event) } doReturn true }
     whenever(eventsPublisher.send(any())).thenThrow(RuntimeException("Boom!"))
@@ -349,5 +356,30 @@ class OutboundEventsServiceTest {
     }
 
     verifyNoMoreInteractions(eventsPublisher)
+  }
+
+
+  @ParameterizedTest
+  @EnumSource(
+    value = OutboundEvent::class,
+    mode = EnumSource.Mode.INCLUDE,
+    names = [
+      "PRISONER_DOMESTIC_STATUS_CREATED",
+      "PRISONER_DOMESTIC_STATUS_UPDATED",
+      "PRISONER_DOMESTIC_STATUS_DELETED"]
+  )
+  fun `should trap exception sending event for given`(event: OutboundEvent) {
+    featureSwitches.stub { on { isEnabled(event) } doReturn true }
+    whenever(eventsPublisher.send(any())).thenThrow(RuntimeException("Boom!"))
+
+    outboundEventsService.send(outboundEvent = event,
+      identifier = 1L,
+      additionalInformation = PrisonerDomesticStatus(
+        domesticStatusId = 1L,
+        domesticStatusCode = "D",
+        source = Source.NOMIS,
+      ),
+    )
+      verify(eventsPublisher).send(any())
   }
 }
