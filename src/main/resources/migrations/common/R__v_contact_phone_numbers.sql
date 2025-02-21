7d2ffddcbb50f9cd062f1cd93d8e15e31fa19659
James Baker <james.baker3@digital.justice.gov.uk>
2025-02-21 12:24:59 +0000

    PRC-489: Fix migrations
    All the views rely on contact_identity or reference_codes so need to be dropped and recreated.

--- src/main/resources/migrations/common/R__v_contact_phone_numbers.sql
+++ src/main/resources/migrations/common/R__v_contact_phone_numbers.sql
@@ -2,7 +2,7 @@
 -- Creates a view over the contact_phone and reference_codes tables to return a list of all phone numbers with descriptions
 -- for all codes.
 -- Note: the view is only dropped if the checksum of this migration changes
--- Internal version to bump if you need to force recreation: 2
+-- Internal version to bump if you need to force recreation: 3
 --
 DROP VIEW IF EXISTS v_contact_phone_numbers;
 CREATE VIEW v_contact_phone_numbers
