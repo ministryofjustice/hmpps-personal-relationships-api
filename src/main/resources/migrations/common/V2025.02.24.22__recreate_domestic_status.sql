00f472bfbdd8f98cf53f5c9f0fe0c83c3252e8d0
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-02-24 13:45:32 +0000

    PRC-426-fix build failure

--- src/main/resources/migrations/common/V2025.02.24.22__recreate_domestic_status.sql
+++ src/main/resources/migrations/common/V2025.02.24.22__recreate_domestic_status.sql
@@ -1,4 +1,4 @@
 --
 -- alter  domestic status
 --
-ALTER TABLE prisoner_domestic_status ALTER COLUMN domestic_status_code TYPE VARCHAR(20);
+ALTER TABLE prisoner_domestic_status ALTER COLUMN domestic_status_code TYPE VARCHAR(12);
