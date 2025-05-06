cbc79d85ca797dd5d522551e808fb34125b78726
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-05-06 14:13:08 +0100

    PRC-588 : update sentry sampling rate

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/config/SentryConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/config/SentryConfig.kt
@@ -13,38 +13,40 @@ private const val PRISONER_CONTACT = "/prisoner/[A-Z0-9]+/contact"
 @Configuration
 class SentryConfig {
 
-    @Bean
-    fun ignoreHealthRequests() = SentryOptions.BeforeSendTransactionCallback { transaction, _ ->
-        transaction.transaction?.let { transactionName ->
-            when {
-                transactionName.startsWith("GET /health") -> null
-                transactionName.startsWith("GET /info") -> null
-                else -> transaction
-            }
-        } ?: transaction
+  @Bean
+  fun ignoreHealthRequests() = SentryOptions.BeforeSendTransactionCallback { transaction, _ ->
+    transaction.transaction?.let { transactionName ->
+      when {
+        transactionName.startsWith("GET /health") -> null
+        transactionName.startsWith("GET /info") -> null
+        else -> transaction
+      }
+    } ?: transaction
+  }
+
+  @Bean
+  fun transactionSampling(): SentryOptions.TracesSamplerCallback = SentryOptions.TracesSamplerCallback { context ->
+    val request = context.customSamplingContext?.get("request") as? HttpServletRequest
+
+    val uri = request?.requestURI.orEmpty()
+    val method = request?.method.orEmpty()
+
+    when {
+      method == "GET" &&
+        (
+          uri.matches(PRISONER_CONTACT.toRegex()) ||
+            uri.matches(CONTACT_RECONCILE.toRegex()) ||
+            uri.matches(PRISONER_NUMBER_OF_CHILDREN.toRegex()) ||
+            uri.matches(PRISONER_DOMESTIC_STATUS.toRegex())
+          ) -> 0.0025
+
+      method == "PUT" &&
+        (
+          uri.matches(PRISONER_NUMBER_OF_CHILDREN.toRegex()) ||
+            uri.matches(PRISONER_DOMESTIC_STATUS.toRegex())
+          ) -> 0.0025
+
+      else -> 0.05
     }
-
-    @Bean
-    fun transactionSampling(): SentryOptions.TracesSamplerCallback = SentryOptions.TracesSamplerCallback { context ->
-        val request = context.customSamplingContext?.get("request") as? HttpServletRequest
-
-        val uri = request?.requestURI.orEmpty()
-        val method = request?.method.orEmpty()
-
-        when {
-            method == "GET" && (
-                    uri.matches(PRISONER_CONTACT.toRegex()) ||
-                            uri.matches(CONTACT_RECONCILE.toRegex()) ||
-                            uri.matches(PRISONER_NUMBER_OF_CHILDREN.toRegex()) ||
-                            uri.matches(PRISONER_DOMESTIC_STATUS.toRegex())
-                    ) -> 0.0025
-
-            method == "PUT" && (
-                    uri.matches(PRISONER_NUMBER_OF_CHILDREN.toRegex()) ||
-                            uri.matches(PRISONER_DOMESTIC_STATUS.toRegex())
-                    ) -> 0.0025
-
-            else -> 0.05
-        }
-    }
+  }
 }
