059d9764a8d6a5dbe757668cbd0878d81ce3efbc
Prabash Balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-05-07 11:04:24 +0100

    PRC-588 : update sentry sampling rate (#122)
    * PRC-588 : update sentry sampling rate

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/config/SentryConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/config/SentryConfig.kt
@@ -1,11 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppscontactsapi.config
 
 import io.sentry.SentryOptions
-import jakarta.persistence.EntityNotFoundException
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
 @Configuration
 class SentryConfig {
+
   @Bean
   fun ignoreHealthRequests() = SentryOptions.BeforeSendTransactionCallback { transaction, _ ->
     transaction.transaction?.let { transactionName ->
@@ -15,13 +16,5 @@ class SentryConfig {
         else -> transaction
       }
     } ?: transaction
-  }
-
-  @Bean
-  fun ignoreEntityNotFoundExceptions() = SentryOptions.BeforeSendCallback { event, _ ->
-    when {
-      event.exceptions?.any { it.type == EntityNotFoundException::class.qualifiedName } == true -> null
-      else -> event
-    }
   }
 }
