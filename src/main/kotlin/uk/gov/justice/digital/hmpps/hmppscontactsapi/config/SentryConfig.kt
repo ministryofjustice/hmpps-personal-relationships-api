930faf5fb19d69e572d56d6495c214f4a3879c6c
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-05-07 07:21:24 +0100

    PRC-588 : update sentry sampling rate

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/config/SentryConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/config/SentryConfig.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppscontactsapi.config
 
 import io.sentry.SentryOptions
-import jakarta.servlet.http.HttpServletRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -29,32 +28,6 @@ class SentryConfig {
     when {
       event.exceptions?.any { it.type == "EntityNotFoundException" } == true -> null
       else -> event
-    }
-  }
-
-  @Bean
-  fun transactionSampling(): SentryOptions.TracesSamplerCallback = SentryOptions.TracesSamplerCallback { context ->
-    val request = context.customSamplingContext?.get("request") as? HttpServletRequest
-
-    val uri = request?.requestURI.orEmpty()
-    val method = request?.method.orEmpty()
-
-    when {
-      method == "GET" &&
-        (
-          uri.matches(PRISONER_CONTACT.toRegex()) ||
-            uri.matches(CONTACT_RECONCILE.toRegex()) ||
-            uri.matches(PRISONER_NUMBER_OF_CHILDREN.toRegex()) ||
-            uri.matches(PRISONER_DOMESTIC_STATUS.toRegex())
-          ) -> 0.0025
-
-      method == "PUT" &&
-        (
-          uri.matches(PRISONER_NUMBER_OF_CHILDREN.toRegex()) ||
-            uri.matches(PRISONER_DOMESTIC_STATUS.toRegex())
-          ) -> 0.0025
-
-      else -> 0.05
     }
   }
 }
