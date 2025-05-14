ae3f8e23e57e375223e43da6c5a916ed47e39455
Prabash Balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-05-14 12:38:09 +0100

    PRC-548 : performance tests for create or update and read (#138)
    PRC-548 : performance tests for create or update and read (#138)

---
+++ src/gatling/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/simulations/CreateOrUpdatePrisonerNumberOfChildrenSimulation.kt
@@ -0,0 +1,65 @@
+package uk.gov.justice.digital.hmpps.hmppscontactsapi.simulations
+
+import io.gatling.javaapi.core.CoreDsl.StringBody
+import io.gatling.javaapi.core.CoreDsl.constantUsersPerSec
+import io.gatling.javaapi.core.CoreDsl.csv
+import io.gatling.javaapi.core.CoreDsl.exec
+import io.gatling.javaapi.core.CoreDsl.jsonPath
+import io.gatling.javaapi.core.CoreDsl.scenario
+import io.gatling.javaapi.http.HttpDsl.http
+import io.gatling.javaapi.http.HttpDsl.status
+import java.time.Duration.ofSeconds
+
+class CreateOrUpdatePrisonerNumberOfChildrenSimulation : BaseSimulation() {
+
+  private val prisonerNumberFeeder = csv("data/prisoner-numbers-$environment.csv").random()
+
+  private fun callGetNumberOfChildren() = exec(
+    http("Get Prisoner Number of Children")
+      .get("/prisoner/#{prisonerNumber}/number-of-children")
+      .headers(authorisationHeader)
+      .check(status().shouldBe(200))
+      .check(jsonPath("$.id").exists().saveAs("id")),
+  )
+  private fun callCreateOrUpdateNumberOfChildren() = exec(
+    http("Create or Update Prisoner Number of Children")
+      .put("/prisoner/#{prisonerNumber}/number-of-children")
+      .body(
+        StringBody {
+          """
+          {
+            "numberOfChildren": 2
+          }
+          """.trimIndent()
+        },
+      ).asJson()
+      .headers(authorisationHeader)
+      .check(status().`in`(200, 409)),
+  )
+
+  private val numberOfChildrenScenario = scenario("Create or Update Prisoner number of children")
+    .exec(getToken)
+    .feed(prisonerNumberFeeder.circular())
+    .repeat(testRepeat)
+    .on(
+      exec(
+        callCreateOrUpdateNumberOfChildren(),
+        callGetNumberOfChildren(),
+      )
+        .pause(ofSeconds(testPauseRangeMin.toLong()), ofSeconds(testPauseRangeMax.toLong())),
+    )
+
+  init {
+    setUp(
+      numberOfChildrenScenario.injectOpen(
+        constantUsersPerSec(userCount.toDouble())
+          .during(ofSeconds(testDuration.toLong()))
+          .randomized(),
+      ),
+    ).protocols(httpProtocol)
+      .assertions(
+        responseTimePercentile3,
+        successfulRequestsPercentage,
+      )
+  }
+}
