0cb329e0beede99ebf0c627769fafa1935e397db
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2025-05-17 18:16:36 +0100

    PRC-740: Prisoner-based reconciliation integration tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/integration/resource/sync/SyncReconcileIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/integration/resource/sync/SyncReconcileIntegrationTest.kt
@@ -3,11 +3,13 @@ package uk.gov.justice.digital.hmpps.hmppscontactsapi.integration.resource.sync
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.groups.Tuple
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.springframework.test.context.jdbc.Sql
 import uk.gov.justice.digital.hmpps.hmppscontactsapi.integration.PostgresIntegrationTestBase
 import uk.gov.justice.digital.hmpps.hmppscontactsapi.model.response.sync.SyncContactReconcile
+import uk.gov.justice.digital.hmpps.hmppscontactsapi.model.response.sync.SyncPrisonerReconcile
 import uk.gov.justice.digital.hmpps.hmppscontactsapi.util.StubUser
 import java.time.LocalDate
 
@@ -20,114 +22,260 @@ class SyncReconcileIntegrationTest : PostgresIntegrationTestBase() {
 
   @BeforeEach
   fun resetEvents() {
-    stubEvents.reset()
     setCurrentUser(StubUser.SYNC_AND_MIGRATE_USER)
   }
 
-  @Test
-  fun `should return unauthorized if no token provided`() {
-    webTestClient.get()
-      .uri("/sync/contact/1/reconcile")
-      .accept(MediaType.APPLICATION_JSON)
-      .exchange()
-      .expectStatus()
-      .isUnauthorized
-  }
+  @Nested
+  inner class ReconciliationByContactTests {
 
-  @Test
-  fun `should return forbidden if incorrect role`() {
-    setCurrentUser(StubUser.USER_WITH_WRONG_ROLES)
-    webTestClient.get()
-      .uri("/sync/contact/1/reconcile")
-      .accept(MediaType.APPLICATION_JSON)
-      .headers(setAuthorisationUsingCurrentUser())
-      .exchange()
-      .expectStatus()
-      .isForbidden
-  }
+    @Test
+    fun `should return unauthorized if no token provided`() {
+      webTestClient.get()
+        .uri("/sync/contact/1/reconcile")
+        .accept(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
 
-  @Test
-  fun `reconcile should return the correct details for the contact`() {
-    val reconcileResponse = webTestClient.get()
-      .uri("/sync/contact/30001/reconcile")
-      .accept(MediaType.APPLICATION_JSON)
-      .headers(setAuthorisationUsingCurrentUser())
-      .exchange()
-      .expectStatus()
-      .isOk
-      .expectHeader().contentType(MediaType.APPLICATION_JSON)
-      .expectBody(SyncContactReconcile::class.java)
-      .returnResult().responseBody!!
+    @Test
+    fun `should return forbidden if incorrect role`() {
+      setCurrentUser(StubUser.USER_WITH_WRONG_ROLES)
+      webTestClient.get()
+        .uri("/sync/contact/1/reconcile")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisationUsingCurrentUser())
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
 
-    with(reconcileResponse) {
-      assertThat(contactId).isEqualTo(30001)
-      assertThat(firstName).isEqualTo("John")
-      assertThat(lastName).isEqualTo("Ma")
-      assertThat(middleNames).isNullOrEmpty()
-      assertThat(dateOfBirth).isEqualTo(LocalDate.of(2000, 11, 21))
+    @Test
+    fun `reconcile should return the correct details for the contact`() {
+      val reconcileResponse = webTestClient.get()
+        .uri("/sync/contact/30001/reconcile")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisationUsingCurrentUser())
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+        .expectBody(SyncContactReconcile::class.java)
+        .returnResult().responseBody!!
 
-      assertThat(phones).hasSize(2)
-      assertThat(phones).extracting("phoneType", "phoneNumber").containsAll(
-        listOf(
-          Tuple("MOB", "01111 666666"),
-          Tuple("HOME", "01111 777777"),
-        ),
-      )
+      with(reconcileResponse) {
+        assertThat(contactId).isEqualTo(30001)
+        assertThat(firstName).isEqualTo("John")
+        assertThat(lastName).isEqualTo("Ma")
+        assertThat(middleNames).isNullOrEmpty()
+        assertThat(dateOfBirth).isEqualTo(LocalDate.of(2000, 11, 21))
 
-      assertThat(emails).hasSize(2)
-      assertThat(emails).extracting("emailAddress").contains("mr.last@example.com", "miss.last@example.com")
-
-      assertThat(identities).hasSize(2)
-      assertThat(identities).extracting("identityType").contains("DL", "PASS")
-
-      assertThat(employments).hasSize(1)
-      assertThat(employments).extracting("organisationId").containsOnly(57L)
-
-      assertThat(restrictions).hasSize(2)
-      assertThat(restrictions).extracting("restrictionType").contains("ACC", "BAN")
-
-      assertThat(addresses).hasSize(3)
-
-      assertThat(relationships).hasSize(4)
-      assertThat(relationships).extracting("prisonerNumber", "contactType", "relationshipType")
-        .containsAll(
+        assertThat(phones).hasSize(2)
+        assertThat(phones).extracting("phoneType", "phoneNumber").containsAll(
           listOf(
-            Tuple("A3333AA", "S", "BRO"),
-            Tuple("A3333AA", "O", "POL"),
-            Tuple("A4444AA", "S", "MOT"),
-            Tuple("A4444AA", "S", "SIS"),
+            Tuple("MOB", "01111 666666"),
+            Tuple("HOME", "01111 777777"),
           ),
         )
 
-      addresses.map { address ->
-        if (address.contactAddressId == 40003L) {
-          assertThat(address.addressPhones)
-            .extracting("phoneNumber", "extNumber")
-            .containsExactly(Tuple("01111 888888", "+0123"))
+        assertThat(emails).hasSize(2)
+        assertThat(emails).extracting("emailAddress").contains("mr.last@example.com", "miss.last@example.com")
+
+        assertThat(identities).hasSize(2)
+        assertThat(identities).extracting("identityType").contains("DL", "PASS")
+
+        assertThat(employments).hasSize(1)
+        assertThat(employments).extracting("organisationId").containsOnly(57L)
+
+        assertThat(restrictions).hasSize(2)
+        assertThat(restrictions).extracting("restrictionType").contains("ACC", "BAN")
+
+        assertThat(addresses).hasSize(3)
+
+        assertThat(relationships).hasSize(4)
+        assertThat(relationships).extracting("prisonerNumber", "contactType", "relationshipType")
+          .containsAll(
+            listOf(
+              Tuple("A3333AA", "S", "BRO"),
+              Tuple("A3333AA", "O", "POL"),
+              Tuple("A4444AA", "S", "MOT"),
+              Tuple("A4444AA", "S", "SIS"),
+            ),
+          )
+
+        addresses.map { address ->
+          if (address.contactAddressId == 40003L) {
+            assertThat(address.addressPhones)
+              .extracting("phoneNumber", "extNumber")
+              .containsExactly(Tuple("01111 888888", "+0123"))
+          }
+        }
+
+        relationships.map { rel ->
+          when (rel.prisonerContactId) {
+            40001L -> {
+              assertThat(rel.relationshipRestrictions)
+                .extracting("restrictionType", "startDate", "expiryDate")
+                .containsExactly(
+                  Tuple("BAN", LocalDate.of(2025, 1, 1), LocalDate.of(2025, 5, 2)),
+                )
+            }
+
+            40002L -> {
+              assertThat(rel.relationshipRestrictions)
+                .extracting("restrictionType", "startDate", "expiryDate")
+                .containsExactly(
+                  Tuple("BAN", LocalDate.of(2025, 1, 2), LocalDate.of(2025, 5, 3)),
+                )
+            }
+
+            else -> null
+          }
         }
       }
+    }
+  }
 
-      relationships.map { rel ->
-        when (rel.prisonerContactId) {
-          40001L -> {
-            assertThat(rel.relationshipRestrictions)
-              .extracting("restrictionType", "startDate", "expiryDate")
-              .containsExactly(
-                Tuple("BAN", LocalDate.of(2025, 1, 1), LocalDate.of(2025, 5, 2)),
-              )
+  @Nested
+  inner class ReconciliationByPrisonerTests {
+
+    @Test
+    fun `should return unauthorized if no token provided`() {
+      webTestClient.get()
+        .uri("/sync/prisoner/A3333AA/reconcile")
+        .accept(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus()
+        .isUnauthorized
+    }
+
+    @Test
+    fun `should return forbidden if incorrect role`() {
+      setCurrentUser(StubUser.USER_WITH_WRONG_ROLES)
+      webTestClient.get()
+        .uri("/sync/prisoner/A3333AA/reconcile")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisationUsingCurrentUser())
+        .exchange()
+        .expectStatus()
+        .isForbidden
+    }
+
+    @Test
+    fun `should return the relationships and restrictions for a prisoner`() {
+      val reconcileResponse = webTestClient.get()
+        .uri("/sync/prisoner/A3333AA/reconcile")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisationUsingCurrentUser())
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+        .expectBody(SyncPrisonerReconcile::class.java)
+        .returnResult().responseBody!!
+
+      with(reconcileResponse) {
+        assertThat(relationships).hasSize(2)
+        assertThat(relationships)
+          .extracting(
+            "contactId",
+            "prisonerContactId",
+            "firstName",
+            "lastName",
+            "prisonerNumber",
+            "relationshipTypeCode",
+            "relationshipToPrisoner",
+            "nextOfKin",
+            "emergencyContact",
+            "approvedVisitor",
+            "active",
+          )
+          .containsAll(
+            listOf(
+              Tuple(30001L, 40001L, "John", "Ma", "A3333AA", "S", "BRO", false, false, false, true),
+              Tuple(30001L, 40002L, "John", "Ma", "A3333AA", "O", "POL", false, false, false, true),
+            ),
+          )
+
+        relationships.map { rel ->
+          when (rel.prisonerContactId) {
+            40001L -> {
+              assertThat(rel.restrictions)
+                .extracting("restrictionType", "startDate", "expiryDate")
+                .containsExactly(
+                  Tuple("BAN", LocalDate.of(2025, 1, 1), LocalDate.of(2025, 5, 2)),
+                )
+            }
+            40002L -> {
+              assertThat(rel.restrictions)
+                .extracting("restrictionType", "startDate", "expiryDate")
+                .containsExactly(
+                  Tuple("BAN", LocalDate.of(2025, 1, 2), LocalDate.of(2025, 5, 3)),
+                )
+            }
+            else -> null
           }
-
-          40002L -> {
-            assertThat(rel.relationshipRestrictions)
-              .extracting("restrictionType", "startDate", "expiryDate")
-              .containsExactly(
-                Tuple("BAN", LocalDate.of(2025, 1, 2), LocalDate.of(2025, 5, 3)),
-              )
-          }
-
-          else -> null
         }
       }
+    }
+
+    @Test
+    fun `should return relationships without restrictions when none present for a prisoner`() {
+      val reconcileResponse = webTestClient.get()
+        .uri("/sync/prisoner/A4444AA/reconcile")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisationUsingCurrentUser())
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+        .expectBody(SyncPrisonerReconcile::class.java)
+        .returnResult().responseBody!!
+
+      with(reconcileResponse) {
+        assertThat(relationships).hasSize(3)
+        assertThat(relationships)
+          .extracting(
+            "contactId",
+            "prisonerContactId",
+            "firstName",
+            "lastName",
+            "prisonerNumber",
+            "relationshipTypeCode",
+            "relationshipToPrisoner",
+            "nextOfKin",
+            "emergencyContact",
+            "approvedVisitor",
+            "active",
+          )
+          .containsAll(
+            listOf(
+              Tuple(30001L, 40003L, "John", "Ma", "A4444AA", "S", "MOT", false, false, false, true),
+              Tuple(30001L, 40004L, "John", "Ma", "A4444AA", "S", "SIS", false, false, false, true),
+              Tuple(30002L, 40005L, "Jack", "Mb", "A4444AA", "O", "POL", false, false, false, true),
+            ),
+          )
+
+        assertThat(relationships[0].restrictions).isEmpty()
+        assertThat(relationships[1].restrictions).isEmpty()
+        assertThat(relationships[2].restrictions).isEmpty()
+      }
+    }
+
+    @Test
+    fun `should return an empty list of relationships for a prisoner with none`() {
+      val reconcileResponse = webTestClient.get()
+        .uri("/sync/prisoner/A5555AA/reconcile")
+        .accept(MediaType.APPLICATION_JSON)
+        .headers(setAuthorisationUsingCurrentUser())
+        .exchange()
+        .expectStatus()
+        .isOk
+        .expectHeader().contentType(MediaType.APPLICATION_JSON)
+        .expectBody(SyncPrisonerReconcile::class.java)
+        .returnResult().responseBody!!
+
+      assertThat(reconcileResponse.relationships).hasSize(0)
     }
   }
 }
