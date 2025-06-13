7e3d262926ebfbe593276a8fbf26e011841a6f0f
James Baker <james.baker3@digital.justice.gov.uk>
2025-06-13 11:26:49 +0100

    PRC-807: Remove old reference APIs and tables
    Removed language, city, country and country APIs that were never used, but we kept them around "just in case".
    Now the service is in production and no-one will ever use these, so we can clean up all the code and tables.

---
+++ src/main/resources/migrations/common/V2025.06.13.36__drop_old_reference_tables.sql
@@ -0,0 +1,6 @@
+-- Removing the old style reference tables. All this is in the reference codes table now.
+
+DROP TABLE IF EXISTS city_reference;
+DROP TABLE IF EXISTS country_reference;
+DROP TABLE IF EXISTS county_reference;
+DROP TABLE IF EXISTS language_reference;
