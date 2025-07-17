ff40e72a4a568839575f92ac3b4dab9d068a1937
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-07-17 10:39:42 +0100

    PRC-842 : Change merge endpoint to accept list of changes for the keeping prisoner restrictions

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/model/request/sync/MergePrisonerRestrictionsRequest.kt
@@ -0,0 +1,21 @@
+package uk.gov.justice.digital.hmpps.hmppscontactsapi.model.request.sync
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
+import uk.gov.justice.digital.hmpps.hmppscontactsapi.model.request.migrate.PrisonerRestrictionDetailsRequest
+
+@Schema(description = "The request to merge restrictions from one prisoner to another, resetting the retained prisoner's restrictions to those in the request and deleting all from the removed prisoner.")
+data class MergePrisonerRestrictionsRequest(
+  @Schema(description = "The prisoner number to retain restrictions for", example = "A1234BC", required = true)
+  @field:NotBlank
+  val keepingPrisonerNumber: String,
+
+  @Schema(description = "The prisoner number to remove restrictions from", example = "A1234BD", required = true)
+  @field:NotBlank
+  val removingPrisonerNumber: String,
+
+  @Schema(description = "Restriction records to be set for the retained prisoner", required = true)
+  @field:Valid
+  val restrictions: List<PrisonerRestrictionDetailsRequest>,
+)
