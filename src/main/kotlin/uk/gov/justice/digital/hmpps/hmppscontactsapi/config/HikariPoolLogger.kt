f97fbab7e1a0fa4ad057524caa75b40e449e0825
Prabash Balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-08-20 17:12:38 +0100

    PRC-899-trigger-alert-for-idel-db-connections (#225)
    * PRC-899 : trigger alert on idle db connections
    
    * PRC-899 : trigger alert on idle db connections + write connection stats periodically to assist further investigation
    
    * PRC-899 : trigger alert on idle db connections + write connection stats periodically to assist further investigation + mini idle connection and keep live time
    
    * PRC-899 : trigger alert on idle db connections + write connection stats periodically to assist further investigation + mini idle connection and keep live time

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/config/HikariPoolLogger.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.hmppscontactsapi.config
+
+import com.zaxxer.hikari.HikariDataSource
+import com.zaxxer.hikari.HikariPoolMXBean
+import jakarta.annotation.PostConstruct
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+@Profile("dev", "preprod") // Only active in dev or preprod
+class HikariPoolLogger(private val dataSource: HikariDataSource) {
+
+  @PostConstruct
+  fun startLogging() {
+    val poolProxy: HikariPoolMXBean = dataSource.hikariPoolMXBean
+    val timer = Timer(true)
+    timer.scheduleAtFixedRate(
+      object : TimerTask() {
+        override fun run() {
+          println(
+            "HikariCP Pool Stats - Total: ${poolProxy.totalConnections}, " +
+              "Active: ${poolProxy.activeConnections}, " +
+              "Idle: ${poolProxy.idleConnections}, " +
+              "Waiting: ${poolProxy.threadsAwaitingConnection}",
+          )
+        }
+      },
+      0,
+      1800000,
+    ) // every 30 minutes
+  }
+}
