b590254501e567b8b43a3f263ae4cb3b7dcf7ca3
Prabash Balasuriya <prabash.balasuriya@justice.gov.uk>
2025-11-28 17:24:09 +0000

    PRC-951: Searching when there has been a name change (#299)
    Co-authored-by: prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscontactsapi/entity/ContactEntityAudit.kt
@@ -0,0 +1,31 @@
+package uk.gov.justice.digital.hmpps.hmppscontactsapi.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import jakarta.persistence.EmbeddedId
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+
+@Embeddable
+data class ContactAuditPk(
+  @Column(name = "contact_id")
+  val contactId: Long,
+  @Column(name = "rev_id")
+  val revId: Long,
+)
+
+@Entity
+@Table(name = "contact_audit")
+data class ContactEntityAudit(
+  @EmbeddedId
+  val id: ContactAuditPk,
+
+  @Column(name = "last_name")
+  val lastName: String,
+
+  @Column(name = "first_name")
+  val firstName: String,
+
+  @Column(name = "middle_names")
+  val middleNames: String?,
+)
