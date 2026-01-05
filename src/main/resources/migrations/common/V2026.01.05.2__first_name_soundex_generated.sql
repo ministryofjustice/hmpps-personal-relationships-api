d594debeb7a4146a89386197c3284d293ffc9263
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2026-01-05 16:36:48 +0000

    PRC-1024: Removed index create

--- src/main/resources/migrations/common/V2026.01.05.2__first_name_soundex_generated.sql
+++ src/main/resources/migrations/common/V2026.01.05.2__first_name_soundex_generated.sql
@@ -1,3 +1,1 @@
 ALTER TABLE contact ADD COLUMN first_name_soundex CHAR(4) GENERATED ALWAYS AS (soundex(first_name)) STORED;
-
-create index concurrently idx_contact_first_name_soundex on contact (first_name_soundex);
