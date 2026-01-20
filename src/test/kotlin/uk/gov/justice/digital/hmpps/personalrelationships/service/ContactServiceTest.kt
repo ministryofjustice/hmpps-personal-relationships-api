1bdfce737344f0edd039ed0b36a907e4c299c9bd
sandeep.khajuria <sankhajuria@gmail.com>
2026-01-20 11:54:31 +0000

    PRC-989 - change package from contacts to personalrelationships

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personalrelationships/service/ContactServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personalrelationships/service/ContactServiceTest.kt
@@ -1414,8 +1414,7 @@ class ContactServiceTest {
         "true,false, Admin",
         "false,true, null",
         "false,false,null",
-
-        )
+      )
       fun `should update the approved visitor , approved by user and approved time`(updatingApprovedVisitor: Boolean, savedApprovedVisitorValue: Boolean, expectedApprovedBy: String) {
         val request = PatchRelationshipRequest(
           isApprovedVisitor = JsonNullable.of(updatingApprovedVisitor),
