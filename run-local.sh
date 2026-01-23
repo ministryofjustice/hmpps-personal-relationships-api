57b14a88cc78da9e1130e5675cbaae5154b29f70
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2026-01-23 12:22:05 +0000

    PRC-944: Clearer instructions and configurable DB port for run modes (test, local, CircleCI and K8s envs) (#340)
    * PRC-944: Clearer instructions and configurable DB port for run modes - test, local, CircleCI and k8s environments
    
    * Use custom ports for local db and integration test db
    
    Defaults back to 5432 if nothing is found in the .env file
    for either of the DB configs.
    
    * format env file readme contents
    
    * Update configuration comments and README to mention new configuration DB ports
    
    also update var name as per PR comments to POSTGRES_TEST_DB_PORT
    
    ---------
    
    Co-authored-by: adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>

--- run-local.sh
+++ run-local.sh
@@ -7,8 +7,14 @@
 # The environment variables here will also override values supplied in spring profile properties, specifically
 # around setting the DB properties, SERVER_PORT and client credentials to match those used in the docker-compose files.
 #
+# The default port for the local postgresq docker container is the standard 5432/tcp, but can be overridden here by
+# setting the LOCAL_DB_PORT environment variable. e.g. `export LOCAL_DB_PORT=5555`.
 
-# Provide the DB connection details to local container-hosted Postgresql DB already running
+# The default port for the integration test postgres container is the standard 5432/tcp, but you can also override this
+# by setting the POSTGRES_TEST_DB_PORT environment variable. e.g. `export POSTGRES_TEST_DB_PORT=9559`. You may need to
+# do this if you have an existing running database on the default port, on your machine.
+
+# Provide the DB connection details to local docker Postgresql DB already running
 export DB_SERVER=localhost
 export DB_NAME=personal-relationships-db
 export DB_USER=contacts
--- src/test/kotlin/uk/gov/justice/digital/hmpps/personalrelationships/integration/container/PostgresContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personalrelationships/integration/container/PostgresContainer.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.personalrelationships.integration.container
 
+import io.github.cdimascio.dotenv.dotenv
 import org.slf4j.LoggerFactory
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.containers.output.Slf4jLogConsumer
@@ -8,8 +9,23 @@ import java.io.IOException
 import java.net.ServerSocket
 
 object PostgresContainer {
+  // Override the external container port if the environment variable exists - otherwise default to 5432/tcp
+  private val dotenv = dotenv {
+    ignoreIfMissing = true // don't crash if .env is absent
+  }
+  val hostPort: Int =
+    dotenv["POSTGRES_TEST_DB_PORT"]?.toInt()
+      ?: System.getenv("POSTGRES_TEST_DB_PORT")?.toInt()
+      ?: 5432
+
+  // Binding externalPort:internalContainer port
+  private val bindings = listOf("$hostPort:5432")
+
   private val log = LoggerFactory.getLogger(this::class.java)
+
   val instance: PostgreSQLContainer<Nothing>? by lazy { startPostgresqlIfNotRunning() }
+
+  // This only starts a TestContainer if postgresql is not already running
   private fun startPostgresqlIfNotRunning(): PostgreSQLContainer<Nothing>? {
     if (isPostgresRunning()) {
       return null
@@ -19,18 +35,21 @@ object PostgresContainer {
 
     return PostgreSQLContainer<Nothing>("postgres:16").apply {
       withEnv("HOSTNAME_EXTERNAL", "localhost")
+      portBindings = bindings
       withDatabaseName("contacts")
       withUsername("contacts")
       withPassword("contacts")
-      setWaitStrategy(Wait.forListeningPort())
       withReuse(false)
+      setWaitStrategy(Wait.forListeningPort())
       start()
       followOutput(logConsumer)
     }
   }
 
+  // This checks whether there is a running instance on the default 5432/tcp port - for example - when
+  // running integration tests within a CircleCI pipelines (here it pre-starts a postgres docker container)
   private fun isPostgresRunning(): Boolean = try {
-    val serverSocket = ServerSocket(5432)
+    val serverSocket = ServerSocket(hostPort)
     serverSocket.localPort == 0
   } catch (e: IOException) {
     true
