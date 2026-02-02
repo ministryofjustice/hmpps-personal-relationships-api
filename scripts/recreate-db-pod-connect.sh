95690ca3b8e519950411462f8afcd474b5dfbaa8
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2026-02-02 15:04:35 +0000

    PRC-952 : Contact search: sounds like option, needs a hard limit to 2000 records to improve performance

--- scripts/recreate-db-pod-connect.sh
+++ scripts/recreate-db-pod-connect.sh
@@ -23,22 +23,7 @@
   # Get the database credentials and RDS instance address from the Kubernetes secret
   read -r database_name database_password database_username rds_instance_address < <(echo $(kubectl -n $NAMESPACE get secrets rds-postgresql-instance-output -o json | jq '.data[] |= @base64d' | jq -r '.data.database_name, .data.database_password, .data.database_username, .data.rds_instance_address'))
   # Always delete the port-forward pod if it exists
-  if kubectl -n $NAMESPACE get pod port-forward-pod &>/dev/null; then
-    kubectl -n $NAMESPACE delete pod port-forward-pod
-    echo "Deleted existing port-forward-pod"
-  fi
-
-  # Create the port-forward pod
-  kubectl -n $NAMESPACE run port-forward-pod --image=ministryofjustice/port-forward --env="REMOTE_HOST=$rds_instance_address" --env="REMOTE_PORT=5432" --env="LOCAL_PORT=5432"
-  # Wait for the pod to be ready
-  kubectl -n $NAMESPACE wait --for=condition=Ready pod/port-forward-pod --timeout=60s
-  echo "port-forward-pod created"
-
-
-  # Kill any existing processes using port 5433
-  if lsof -ti :5433 >/dev/null; then
-    lsof -ti :5433 | xargs -r kill
-  fi
-
-  # Start port-forwarding
-  kubectl -n $NAMESPACE port-forward port-forward-pod 5433:5432
+  echo $database_name
+  echo $database_username
+  echo $database_password
+  echo $rds_instance_address
