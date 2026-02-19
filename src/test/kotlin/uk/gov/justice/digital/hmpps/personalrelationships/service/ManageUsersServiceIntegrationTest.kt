cb04e2fc77959997591b67e0d2d2c2afcda47484
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-02-19 10:00:45 +0000

    After Java 25 upgrade, ManageUsersApiClient was broken (null mock) - Fix
    This seems to be due to a test class mocking the web client, and that
    mock is leaking into other tests via a shared test context.
    
    Update the class mocking the client to:
    
    1. Only mock within it's own test context, the client. Does not effect
    other test classes anymore.
    
    2. Reset data between tests, as it seems the cache was being shared between them all,
    corrupting the tests.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/personalrelationships/service/ManageUsersServiceIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/personalrelationships/service/ManageUsersServiceIntegrationTest.kt
@@ -1,9 +1,10 @@
 package uk.gov.justice.digital.hmpps.personalrelationships.service
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mockito.mock
+import org.mockito.Mockito
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
@@ -12,38 +13,41 @@ import org.springframework.cache.CacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import uk.gov.justice.digital.hmpps.personalrelationships.client.manage.users.ManageUsersApiClient
 import uk.gov.justice.digital.hmpps.personalrelationships.client.manage.users.UserDetails
 import uk.gov.justice.digital.hmpps.personalrelationships.config.CacheConfiguration
 
 @ExtendWith(SpringExtension::class)
-@ContextConfiguration(classes = [CacheConfiguration::class, ManageUsersServiceIntegrationTest.ManageUsersServiceIntegrationTestConfiguration::class])
+@ContextConfiguration(classes = [CacheConfiguration::class, ManageUsersServiceIntegrationTest.TestConfig::class])
 class ManageUsersServiceIntegrationTest {
 
-  @Configuration
-  class ManageUsersServiceIntegrationTestConfiguration {
-    @Bean
-    fun manageUsersService(manageUsersApiClient: ManageUsersApiClient): ManageUsersService = ManageUsersService(manageUsersApiClient)
-
-    @Bean
-    fun manageUsersApiClient(): ManageUsersApiClient = mock()
+  @BeforeEach
+  fun setUp() {
+    cacheManager.getCache(CacheConfiguration.USER_DETAILS_CACHE)?.clear()
+    Mockito.reset(manageUsersApiClient)
   }
 
   @Autowired
   private lateinit var manageUsersService: ManageUsersService
 
   @Autowired
+  private lateinit var cacheManager: CacheManager
+
+  @MockitoBean
   private lateinit var manageUsersApiClient: ManageUsersApiClient
 
-  @Autowired
-  private lateinit var cacheManager: CacheManager
+  @Configuration
+  class TestConfig {
+    @Bean
+    fun manageUsersService(manageUsersApiClient: ManageUsersApiClient): ManageUsersService = ManageUsersService(manageUsersApiClient)
+  }
 
   @Test
   fun `should cache requests for users`() {
     val user = UserDetails("FOO", "Foo User")
-
-    whenever(manageUsersApiClient.getUserByUsername("FOO")).thenReturn(UserDetails("FOO", "Foo User"))
+    whenever(manageUsersApiClient.getUserByUsername("FOO")).thenReturn(user)
 
     assertThat(manageUsersService.getUserByUsername("FOO")).isEqualTo(user)
     assertThat(manageUsersService.getUserByUsername("FOO")).isEqualTo(user)
@@ -54,8 +58,7 @@ class ManageUsersServiceIntegrationTest {
   @Test
   fun `should cache again after eviction`() {
     val user = UserDetails("FOO", "Foo User")
-
-    whenever(manageUsersApiClient.getUserByUsername("FOO")).thenReturn(UserDetails("FOO", "Foo User"))
+    whenever(manageUsersApiClient.getUserByUsername("FOO")).thenReturn(user)
 
     assertThat(manageUsersService.getUserByUsername("FOO")).isEqualTo(user)
 
