1a5f9a3bd5ac8d1288f2108c508d7123dbdb94d6
Sorrel Lovell <sorrel.lovell@opencastsoftware.com>
2026-06-29 10:20:53 +0100

    Fix indentation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/personalrelationships/repository/PrisonerContactSearchRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/personalrelationships/repository/PrisonerContactSearchRepository.kt
@@ -67,14 +67,14 @@ class PrisonerContactSearchRepository(
     if (pageable.sort.isSorted) {
       val order = pageable.sort.map {
         val property = mapSortPropertiesOfPrisonerContactSearch(it.property)
-        var order: Order = if (it.isAscending) {
+        val order: Order = if (it.isAscending) {
           cb.asc(contact.get<String>(property))
         } else {
           cb.desc(contact.get<String>(property))
         }
         // always order date of birth with nulls last
         if (property == PrisonerContactSummaryEntity::dateOfBirth.name && order is JpaOrder) {
-            order.nullPrecedence(Nulls.LAST)
+          order.nullPrecedence(Nulls.LAST)
         }
         order
       }.toList()
